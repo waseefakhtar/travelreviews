1c141fd089065b01eb19d9eeb3d2484bc59c8c3f
Waseef Akhtar <waseef@degoo.com>
2020-09-12 01:15:10 +0500

    Fixing java.lang.IllegalStateException: new updateScope not found in result type of endRestartGroup

--- app/src/main/java/com/example/travel_reviews/MainActivity.kt
+++ app/src/main/java/com/example/travel_reviews/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.travel_reviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.Text
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*setContent {
+            TravelReviewsApp()
+        }*/
     }
+}
+
+@Composable
+fun TravelReviewsApp() {
+    /*Scaffold(
+        topBar = {
+            TravelReviewsTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { screen -> currentScreen = screen },
+                currentScreen = currentScreen
+            )
+        }
+    )*/
+    Text(text = "Hello")
 }
---
+++ app/src/main/java/com/example/travel_reviews/ui/components/TopAppBar.kt
@@ -0,0 +1,8 @@
+package com.example.travel_reviews.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TravelReviewsTopAppBar() {
+
+}
